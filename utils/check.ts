80c59847ed90ff972afd1ab06b1e145603790d76
Nithin <nithinkatkam504106@gmail.com>
2024-04-20 00:57:40 +0530

    check api

---
+++ utils/check.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+export async function checkApi(
+   tableName:string,
+    creatorAddress: string
+  ) {
+    try {
+        const apiUrl = `https://testnets.tableland.network/api/v1/query?statement=select%20%2A%20from%20${tableName}`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+        const filteredData = data.filter((item: {  
+          address: string;
+            creator: string;
+            id: number;
+            metadata: string;
+            price?: number; 
+            streamId?: string;
+            title: string; })=> item.creator === creatorAddress);
+        return filteredData;
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+        return null;
+      }
+    }
